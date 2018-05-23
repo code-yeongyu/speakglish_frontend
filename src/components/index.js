337c0c987d0b65c4773852ee6dd7c3c54be797fe
kim-yeon-gyu-exlock <public.kim.yeon.gyu@gmail.com>
2018-05-24 00:34:05 +0900

    bug fixed : wasn't able to login with email, wasn't show any animations or alert when have any error on login or register

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export {default as AddArticle} from './AddArticle';
 export {default as Timer} from './Timer';
 export {default as WritingField} from './WritingField';
 export {default as Warning} from './Warning';
+export {default as RegisterWarning} from './RegisterWarning'
+export {default as LoginWarning} from './LoginWarning'

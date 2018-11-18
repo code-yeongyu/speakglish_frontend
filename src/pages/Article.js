2d61f708d67e01c94d5bba920d1b5b0630944208
Yeon-Gyu-Kim <code.yeon.gyu@gmail.com>
2018-11-18 20:49:04 +0900

    Update: Article.js
    speech api 를 annyang에서 webspeechapi 로 변경

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
-import {Header, WritingField} from '../components';
-import {pageRedirection, signOut} from '../util';
-import {URL} from '../config/Api';
-import {connect} from 'react-redux';
-import {profileRequest, logout} from '../modules/account';
-import annyang from 'annyang';
+import React, { Component } from "react";
+import { Header, WritingField } from "../components";
+import { pageRedirection, signOut } from "../util";
+import { URL } from "../config/Api";
+import { connect } from "react-redux";
+import { profileRequest, logout } from "../modules/account";
 
-export class Article extends Component{
+const recognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+export class Article extends Component {
     state = {
         isStarted: false,
         content: "",
@@ -15,18 +16,19 @@ export class Article extends Component{
         articleId: 0,
         isMicWorking: false,
         isTimerWorking: false
-    }
-    constructor(props){
+    };
+    componentDidMount() {}
+    constructor(props) {
         super(props);
-        
-        if(!pageRedirection(this.props, true)){//로그인 되지 않은 상태일시 Home으로 이동
-            fetch(URL+"/api/articles/"+this.props.match.params.articleId, {
-                method:"GET",
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Token "+this.props.store.status.token
-                }
-            })
+
+        pageRedirection(this.props, true); //로그인 되지 않은 상태일시 Home으로 이동
+        fetch(URL + "/api/articles/" + this.props.match.params.articleId, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Token " + this.props.store.status.token
+            }
+        })
             .then(response => response.json())
             .then(json => {
                 this.setState({
@@ -36,45 +38,58 @@ export class Article extends Component{
                     remainingTime: json.timerLeft
                 });
             });
-            annyang.setLanguage('en');
-            annyang.addCallback('result', (heardArray)=>{
-                this.setState({
-                    ...this.state,
-                    content: this.state.content+heardArray[0]
-                })
-                annyang.pause();
-                annyang.start({ autoRestart: true, continuous: true });;
-            })
-            annyang.addCallback('error', function() {
-                console.log("an error ocurred")
+        this.recognition = new recognition();
+        this.recognition.lang = "en-US";
+        this.recognition.onresult = event => {
+            const length = event.results.length;
+            const text = event.results[length - 1][0].transcript;
+            this.setState({
+                ...this.state,
+                content: this.state.content + " " + text
             });
-        }
+        };
+        this.recognition.onend = event => {
+            this.recognition.start();
+        };
     }
-    updateContent = (evt) => {
+
+    updateContent = evt => {
         this.setState({
             ...this.state,
             content: evt.target.value
         });
-    }
+    };
     requestPatch = () => {
-        fetch(URL+"/api/articles/"+this.props.match.params.articleId+"/",{
-            method:"PATCH",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": "Token "+this.props.store.status.token
-            },
-            body: "timer="+this.state.settedTime+"&timerLeft="+this.state.remainingTime+"&content="+this.state.content
-        })
-    }
-    requestDelete = () => {
-        fetch(URL+"/api/articles/"+this.props.match.params.articleId+"/",{
-            method:"DELETE",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": "Token "+this.props.store.status.token
+        fetch(
+            URL + "/api/articles/" + this.props.match.params.articleId + "/",
+            {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    Authorization: "Token " + this.props.store.status.token
+                },
+                body:
+                    "timer=" +
+                    this.state.settedTime +
+                    "&timerLeft=" +
+                    this.state.remainingTime +
+                    "&content=" +
+                    this.state.content
             }
-        })
-    }
+        );
+    };
+    requestDelete = () => {
+        fetch(
+            URL + "/api/articles/" + this.props.match.params.articleId + "/",
+            {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    Authorization: "Token " + this.props.store.status.token
+                }
+            }
+        );
+    };
     pauseInterval = () => {
         this.setState({
             ...this.state,
@@ -82,86 +97,81 @@ export class Article extends Component{
             isTimerWorking: false
         });
         clearInterval(window.timerInterval);
-        annyang.abort();
-    }
+        this.recognition.onend = event => {};
+        this.recognition.stop();
+    };
     resumeInterval = () => {
         this.setState({
             ...this.state,
             isMicWorking: true,
             isTimerWorking: true
         });
-        annyang.start({ autoRestart: true, continuous: true });;
-        window.timerInterval = setInterval(()=>{
-            if(this.state.remainingTime-1 === 0){
-                clearInterval(window.timerInterval);
-                annyang.abort();
-            }
-            this.setState({
-                ...this.state,
-                remainingTime: this.state.remainingTime-1,
-            });
-            if(this.state.remainingTime%30 === 0){
-                this.requestPatch();
-                annyang.abort();
-                annyang.start({ autoRestart: true, continuous: true });;
-            }
-        }, 1000);
-    }
+        this.recognition.onend = event => {
+            this.recognition.start();
+        };
+        this.recognition.start();
+        this.startTimer();
+    };
     pauseMic = () => {
-        annyang.abort()
+        this.recognition.onend = event => {};
+        this.recognition.stop();
         this.setState({
-            isMicWorking:false
+            isMicWorking: false
         });
-    }
+    };
     resumeMic = () => {
-        annyang.start({ autoRestart: true, continuous: true });
+        this.recognition.stop();
         this.setState({
-            isMicWorking:true
+            isMicWorking: true
         });
-    }
-    addAMinute = () => {
-        this.setState({
-            ...this.state,
-            settedTime: this.state.settedTime+60,
-            remainingTime: this.state.remainingTime+60
-        });
-    }
+    };
     removeArticle = () => {
         this.requestDelete();
         this.props.history.push("/articles");
-    }
+    };
     saveArticle = () => {
-        this.requestPatch()
+        this.requestPatch();
         this.props.history.push("/articles");
-    }
+    };
 
-    render (){
+    render() {
         return (
             <div className="write">
-                <Header username={this.props.store.profile.name} logoutFunction={()=>{signOut(this.props)}}/>
+                <Header
+                    username={this.props.store.profile.name}
+                    logoutFunction={() => {
+                        signOut(this.props);
+                    }}
+                />
                 <WritingField
-                    content={this.state.content} settedTime={this.state.settedTime}
-                    remainingTime={this.state.remainingTime} isMicWorking={this.state.isMicWorking}
-                    isTimerWorking={this.state.isTimerWorking} onUpdateContent={this.updateContent}
-                    onPauseInterval={this.pauseInterval} onResumeInterval={this.resumeInterval}
-                    onAddAMinute={this.addAMinute} onSave={this.saveArticle}
+                    content={this.state.content}
+                    settedTime={this.state.settedTime}
+                    remainingTime={this.state.remainingTime}
+                    isMicWorking={this.state.isMicWorking}
+                    isTimerWorking={this.state.isTimerWorking}
+                    onUpdateContent={this.updateContent}
+                    onPauseInterval={this.pauseInterval}
+                    onResumeInterval={this.resumeInterval}
+                    onAddAMinute={this.addAMinute}
+                    onSave={this.saveArticle}
                     onRemove={this.removeArticle}
-                    onPauseMic={this.pauseMic} onResumeMic={this.resumeMic}
+                    onPauseMic={this.pauseMic}
+                    onResumeMic={this.resumeMic}
                 />
             </div>
         );
     }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
         store: state
     };
 };
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
     return {
-        profileRequest: (token) => { 
-            return dispatch(profileRequest(token)); 
+        profileRequest: token => {
+            return dispatch(profileRequest(token));
         },
         logout: () => {
             return dispatch(logout());
@@ -169,4 +179,7 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Article);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Article);
